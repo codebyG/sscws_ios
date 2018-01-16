711be15a613a19724a739a97f044e3d31ca9c3e2
Myeongun Kim <rollkim@Myeonguns-Mac.local>
2018-01-15 20:54:35 -0800

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Myeongun Kim on 15/01/2018.
+//  Copyright © 2018 Myeongun Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
